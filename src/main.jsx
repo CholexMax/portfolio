308b147c783dbf76886cc7d12811b311c35a44af
cholexMax <tobexnewlife@gmail.com>
2024-12-31 12:55:33 +0530

    basic setup completed

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1 className='bg-orange-400'>SUBSCRIBE NOW</h1>
+  </StrictMode>
+)                                                                                                           
